bba0454ee78d456ab506e52cd5c01b43346cdacd
Moreka <mrkazemi95@hotmail.com>
2020-03-28 18:12:16 -0400

    prblem solved!

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -12,28 +12,68 @@ from sklearn.metrics import confusion_matrix
 from sklearn.datasets import load_iris
 
 
+# def fit_NeuralNetwork(X_train, y_train, alpha, hidden_layer_sizes, epochs):
+#     layer_units = ([len(X_train[-1])] + hidden_layer_sizes + [1])
+#     W = [np.random.rand(n_fan_in_ + 1, n_fan_out_) for n_fan_in_, n_fan_out_ in
+#          zip(layer_units[:-1], layer_units[1:])]
+#     W = np.true_divide(W, 10)
+#     X_train = np.insert(X_train, 0, 1, axis=1)
+#     # X_train = np.true_divide(X_train, 10 * len(X_train))
+#     error_list = []
+#     error_dic = {}
+#     all_W = []
+#     for _ in range(epochs):
+#         error_over_epoch = 0
+#         # np.random.shuffle(X_train)
+#         for N, x_n in enumerate(X_train):
+#             X_n, S_n = forwardPropagation(x_n, W)
+#             g_n = backPropagation(X_n, y_train[N], S_n, W)
+#             W = updateWeights(W, g_n, alpha)
+#             all_W.append(W)
+#             error_over_epoch += 1 if pred(x_n, W) != y_train[N] else 0
+#         error_list.append(error_over_epoch / len(X_train))
+#         error_dic[error_over_epoch] = W
+#     # print(error_list)
+#     print(np.subtract(all_W[-1], all_W[0]))
+#     return error_list, W
+
 def fit_NeuralNetwork(X_train, y_train, alpha, hidden_layer_sizes, epochs):
     layer_units = ([len(X_train[-1])] + hidden_layer_sizes + [1])
-    W = [np.random.rand(n_fan_in_ + 1, n_fan_out_) for n_fan_in_, n_fan_out_ in
+    w = [np.ones((n_fan_in_ + 1, n_fan_out_)) for n_fan_in_, n_fan_out_ in
          zip(layer_units[:-1], layer_units[1:])]
-    # W = np.true_divide(W, 10)
+    w = np.true_divide(w, 10)
     X_train = np.insert(X_train, 0, 1, axis=1)
-    error_list = []
-    error_dic = {}
-    all_W = []
-    for _ in range(epochs):
-        error_over_epoch = 0
-        np.random.shuffle(X_train)
-        for N, x_n in enumerate(X_train):
-            X_n, S_n = forwardPropagation(x_n, W)
-            g_n = backPropagation(X_n, y_train[N], S_n, W)
-            W = updateWeights(W, g_n, alpha)
-            all_W.append(W)
-            error_over_epoch += 1 if pred(x_n, W) != y_train[N] else 0
-        error_list.append(error_over_epoch / len(X_train))
-        error_dic[error_over_epoch] = W
-    print(error_list)
-    return error_list, W
+
+    for _ in range(epochs * 5):
+        for data in zip(X_train, y_train):
+            x, y = data
+            if y == -1: y = 0
+            print('x:', x, '\ty:', y)
+            X, S = forwardPropagation(x, w)
+            g, err = backPropagation(X, y, S, w)
+            w = updateWeights(w, err, -alpha)
+    print('done')
+    print(w)
+    return 0, w
+
+# works for XOR
+# def fit_NeuralNetwork(X_train, y_train, alpha, hidden_layer_sizes, epochs):
+#     layer_units = ([len(X_train[-1])] + hidden_layer_sizes + [1])
+#     w = [np.random.rand(n_fan_in_ + 1, n_fan_out_) for n_fan_in_, n_fan_out_ in
+#          zip(layer_units[:-1], layer_units[1:])]
+#
+#     X_train = np.insert(X_train, 0, 1, axis=1)
+#
+#     for _ in range(epochs * 10):
+#         for data in zip(X_train, y_train):
+#             x, y = data
+#             if y == -1: y = 0
+#             print('x:', x, '\ty:', y)
+#             X, S = forwardPropagation(x, w)
+#             g, err = backPropagation(X, y, S, w)
+#             w = updateWeights(w, err, -alpha)
+#     print('done')
+#     return w
 
 
 def forwardPropagation(x, weights):
@@ -54,62 +94,63 @@ def forwardPropagation(x, weights):
             Xl = output_function(Xl_before_activation)
         X.append(Xl)
         S.append(sl)
-    np.delete
     return np.array(X), np.array(S)
 
 
 from copy import deepcopy
+
+
 def backPropagation(X, y_n, s, weights):
-    weights_copy = deepcopy(weights)
+    w = deepcopy(weights)
     g = [None] * len(X)
     X = np.array(X)
     for layer, Xl in enumerate(reversed(X)):
         layer = len(X) - layer - 1
         if layer == len(X) - 1:
-            delta = 2 * (Xl[0] - y_n) * derivativeOutput(s[-1][0])
+            delta = 1 * (y_n - Xl[0]) * derivativeOutput(s[-1][0])
             g[layer] = np.array([delta])
         elif layer > 0:
-            derivatives = np.zeros([len(Xl) - 1, len(Xl) - 1])
-            for i in range(len(Xl) - 1):
-                derivatives[i][i] = derivativeActivation(Xl[i + 1])
-
-            Wl = weights_copy[layer]
-            # Wl = weights[layer]
-            Wl_t = np.array(Wl)
-            g[layer] = np.dot(np.dot(Wl_t, (g[layer + 1]).T)[1:].T, derivatives)
-            # g[layer] = ((Wl_t).dot((g[layer + 1]).T)[1:]).T.dot(derivatives)
+            deltas = []
+            for d in range(len(s[layer - 1])):
+                derivative = derivativeActivation(s[layer - 1][d])
+                sum = 0
+                for k, delta in enumerate(g[layer + 1]):
+                    sum += (delta * w[layer][d + 1][k])
+                deltas.append(sum * derivative)
+            g[layer] = np.array(deltas)
 
     g = g[1:]
 
-    to_update_W = weights_copy
-    # updatedW = weights
+    to_update_W = w
     for layer, Xl in enumerate(X[:-1]):
         to_update_W[layer] = np.dot(np.array([Xl]).T, np.array([g[layer]]))
+    return g, to_update_W
 
-    return (to_update_W)
 
-
-def updateWeights(weights, g, alpha):
-    return np.subtract(np.array(weights), np.multiply(np.array(g), alpha))
+def updateWeights(weights, err, alpha):
+    return np.subtract(np.array(weights), np.multiply(np.array(err), alpha))
+    # return np.add(weights, np.multiply(np.array(err), alpha))
+    # return np.add(weights, np.array(err))
 
 
 def activation(s):
     return 0 if s <= 0 else s
-    # return np.tanh(s)
+    # return 1 / (1 + np.exp(-s))
 
 
 def derivativeActivation(s):
     return 0 if s <= 0 else 1
-    # return (1 - np.tanh(s) ** 2)
+    # return activation(s) * (1 - activation(s))
 
 
 def outputf(s):
-    return (1) / ((1) + np.exp(-s))
+    return (1) / (1 + np.exp(-s))
+    # return np.tanh(s)
 
 
 def derivativeOutput(s):
-    # return (outputf(s)) * (1 - outputf(s))
-    return (np.exp(-s)) / ((1 + np.exp(-s)) ** 2)
+    return (outputf(s)) * (1 - outputf(s))
+    # return 1 / np.cosh(s)
 
 
 def errorf(x_L, y):
@@ -135,6 +176,8 @@ def derivativeError(x_L, y):
 def pred(x_n, weights):
     x, s = forwardPropagation(x_n, weights)
     res = 1 if x[-1][-1] >= 0.5 else -1
+    print('({}, {})'.format(res, x[-1][-1]))
+    # res = 1 if x[-1][-1] >= 0 else -1
     return res
 
 
@@ -196,10 +239,13 @@ def test():
         else:
             y_test[j] = 1
 
-    err, w = fit_NeuralNetwork(X_train, y_train, 1e-1, [2], 10000)
+    # X_train = np.array([[1, 1], [1, 0], [0, 1], [0, 0]])
+    # y_train = [-1, 1, 1, -1]
+    # w = fit_NeuralNetwork(X_train, y_train, 1e1, [3], 100)
+    # cM = confMatrix(X_train, y_train, w)
 
+    err, w = fit_NeuralNetwork(X_train, y_train, 1e-2, [30, 10], 100)
     # plotErr(err, 100)
-
     cM = confMatrix(X_test, y_test, w)
 
     # sciKit = test_SciKit(X_train, X_test, y_train, y_test)
